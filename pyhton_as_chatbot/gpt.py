8892240b3cc487a259caba256f9a6d85c653b518
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-22 16:03:27 +0300

    all version of my need

---
+++ pyhton_as_chatbot/gpt.py
@@ -0,0 +1,26 @@
+import openai
+import gradio as gr
+
+openai.api_key = "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"  
+
+def predict(message, history):
+    history_openai_format = []
+    for human, assistant in history:
+        history_openai_format.append({"role": "user", "content": human })
+        history_openai_format.append({"role": "assistant",  "content": "You are expert of https://hangicheckup.com/ this website and very deep knowledge about check up. If someone asks you about check up, you will help the user. You never say 'you should get support from a health professional' or something like that. Questions that are outside the hangicheckup site and not related to check up, you should say that this is outside my area of competence because you are only a check up assistant."})
+    history_openai_format.append({"role": "user", "content": message})
+
+    response = openai.ChatCompletion.create(
+        model='gpt-4',
+        messages= history_openai_format,
+        temperature=1.0,
+        stream=True
+    )
+
+    partial_message = ""
+    for chunk in response:
+        if len(chunk['choices'][0]['delta']) != 0:
+            partial_message = partial_message + chunk['choices'][0]['delta']['content']
+            yield partial_message
+
+gr.ChatInterface(predict).queue().launch()
---
+++ python/gpt.py
@@ -0,0 +1,22 @@
+import openai
+import gradio
+
+openai.api_key = "sk-***"  
+
+messages = [
+    {"role": "system", "content": "You are expert of https://hangicheckup.com/ this website and very deep knowledge about check up. If someone asks you about check up, you will help the user. You never say 'you should get support from a health professional' or something like that. Questions that are outside the hangicheckup site and not related to check up, you should say that this is outside my area of competence because you are only a check up assistant."}
+]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Hangi Check Up Assistant")
+
+demo.launch(share=True)
